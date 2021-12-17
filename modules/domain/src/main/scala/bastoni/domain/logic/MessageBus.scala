25d440242a55e877074754717fe4a82d9faf5283
Fabio Epifani <epifab@gmail.com>
2021-12-17 09:12:36 +0000

    Generic bus

--- modules/domain/src/main/scala/bastoni/domain/logic/MessageBus.scala
+++ modules/domain/src/main/scala/bastoni/domain/logic/MessageBus.scala
@@ -8,28 +8,35 @@ import cats.syntax.flatMap.toFlatMapOps
 import cats.syntax.functor.toFunctorOps
 import fs2.concurrent.Topic
 
-trait MessageBus[F[_]]:
-  def publish1(message: Message): F[Unit]
-  def publish(messages: fs2.Stream[F, Message]): fs2.Stream[F, Unit]
-  def subscribe: fs2.Stream[F, Message]
-  def subscribeAwait: Resource[F, fs2.Stream[F, Message]]
+
+trait Bus[F[_], A]:
+  def publish1(message: A): F[Unit]
+  def publish(messages: fs2.Stream[F, A]): fs2.Stream[F, Unit]
+  def subscribe: fs2.Stream[F, A]
+  def subscribeAwait: Resource[F, fs2.Stream[F, A]]
   def run: fs2.Stream[F, Unit]
 
-object MessageBus:
 
-  private class MessageBusImpl[F[_]](
-    topic: Topic[F, Message],
-    queue: Queue[F, Message],
-    override val run: fs2.Stream[F, Unit]
-  ) extends MessageBus[F]:
-    def publish1(message: Message): F[Unit] = queue.offer(message)
-    def publish(messages: fs2.Stream[F, Message]): fs2.Stream[F, Unit] = messages.evalMap(publish1)
-    val subscribe: fs2.Stream[F, Message] = topic.subscribe(128)
-    val subscribeAwait: Resource[F, fs2.Stream[F, Message]] = topic.subscribeAwait(128)
+class Fs2Bus[F[_], A] private(
+  topic: Topic[F, A],
+  queue: Queue[F, A],
+  override val run: fs2.Stream[F, Unit]
+) extends Bus[F, A]:
+  def publish1(message: A): F[Unit] = queue.offer(message)
+  def publish(messages: fs2.Stream[F, A]): fs2.Stream[F, Unit] = messages.evalMap(publish1)
+  val subscribe: fs2.Stream[F, A] = topic.subscribe(128)
+  val subscribeAwait: Resource[F, fs2.Stream[F, A]] = topic.subscribeAwait(128)
 
-  def inMemory[F[_]: Concurrent]: F[MessageBus[F]] =
+object Fs2Bus:
+  def apply[F[_]: Concurrent, A]: F[Bus[F, A]] =
     for {
-      topic <- Topic[F, Message]
-      queue <- Queue.bounded[F, Message](128)
+      topic <- Topic[F, A]
+      queue <- Queue.bounded[F, A](128)
       run = topic.publish(fs2.Stream.fromQueueUnterminated(queue))
-    } yield new MessageBusImpl[F](topic, queue, run)
+    } yield new Fs2Bus[F, A](topic, queue, run)
+
+
+type MessageBus[F[_]] = Bus[F, Message]
+
+object MessageBus:
+  def inMemory[F[_]: Concurrent]: F[MessageBus[F]] = Fs2Bus[F, Message]
