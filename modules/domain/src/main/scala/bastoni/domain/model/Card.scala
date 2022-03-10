a26ec5d73c2a5b5b9441557bc9dae4c61282909d
epifab <epifab@gmail.com>
2022-03-10 10:40:16 +0000

    Hidden card should retain previous knowledge

--- modules/domain/src/main/scala/bastoni/domain/model/Card.scala
+++ modules/domain/src/main/scala/bastoni/domain/model/Card.scala
@@ -51,18 +51,20 @@ sealed trait CardInstance extends CardRef:
   def toOption: Option[VisibleCard]
   def isHidden: Boolean = toOption.isEmpty
   def contains(card: SimpleCard): Boolean = toOption.map(_.simple).contains(card)
-  def hidden: HiddenCard = HiddenCard(ref)
+  def hidden: HiddenCard
 
 case class VisibleCard(rank: Rank, suit: Suit, ref: CardId) extends CardInstance with Card:
-  override def toOption: Option[VisibleCard] = Some(this)
   def simple: SimpleCard = SimpleCard(rank, suit)
+  override def toOption: Option[VisibleCard] = Some(this)
+  override def hidden: HiddenCard = HiddenCard(ref, Some(simple))
 
 object VisibleCard:
   given Encoder[VisibleCard] = deriveEncoder
   given Decoder[VisibleCard] = deriveDecoder
 
-case class HiddenCard(ref: CardId) extends CardInstance:
-  override def toOption: Option[VisibleCard] = None
+case class HiddenCard(ref: CardId, card: Option[Card] = None) extends CardInstance:
+  override def toOption: Option[VisibleCard] = card.map(c => VisibleCard(c.rank, c.suit, ref))
+  override def hidden: HiddenCard = this
 
 object HiddenCard:
   given Encoder[HiddenCard] = deriveEncoder
