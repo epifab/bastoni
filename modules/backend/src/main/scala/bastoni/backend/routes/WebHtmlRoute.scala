443ed62a3acfe2f3cbf9a6f14c1865636e53243c
Fabio Epifani <epifab@gmail.com>
2024-02-13 10:34:42 +0000

    Resume scalajs frontend

--- modules/backend/src/main/scala/bastoni/backend/routes/WebHtmlRoute.scala
+++ modules/backend/src/main/scala/bastoni/backend/routes/WebHtmlRoute.scala
@@ -30,14 +30,10 @@ object WebHtmlRoute:
               <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </head>
             <body>
-              <div id="container"></div>
-              <script src="/static/bundle.js"></script>
-              <!--
               <div id="app-wrapper">
                 <h1><img src="/static/denari.svg" alt="Controbuio" class="spinning" /></h1>
               </div>
               <script src={s"/assets/frontend-opt-bundle.js?v=$appVersion"}></script>
-              -->
             </body>
           </html>
         )
