5bd36103899d1d3f7cd250156e0089063866dd9c
Fabio Epifani <epifab@gmail.com>
2024-02-13 10:34:13 +0000

    Removes broken CORS

--- modules/backend/src/main/scala/bastoni/backend/BackendApp.scala
+++ modules/backend/src/main/scala/bastoni/backend/BackendApp.scala
@@ -1,13 +1,11 @@
 package bastoni.backend
 
 import bastoni.backend.routes.{AuthControllerRoutes, GameControllerRoutes, StaticResourceRoute, WebHtmlRoute}
-import bastoni.domain.logic.{GameController, MessageBus, Services}
-import bastoni.domain.model.User
-import cats.effect.{ExitCode, IO, IOApp, Ref, Resource}
-import fs2.concurrent.Signal
+import bastoni.domain.logic.{GameController, Services}
+import cats.effect.{ExitCode, IO, IOApp, Resource}
 import org.http4s.blaze.server.BlazeServerBuilder
-import org.http4s.server.{Router, Server, ServerBuilder}
-import org.http4s.server.middleware.{CORS, CORSConfig, GZip}
+import org.http4s.server.{Router, Server}
+import org.http4s.server.middleware.GZip
 import org.http4s.server.websocket.WebSocketBuilder2
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 import org.typelevel.log4cats.Logger
@@ -19,18 +17,15 @@ object BackendApp extends IOApp:
   private def webServer(gameController: GameController[IO]): Resource[IO, Server] =
     BlazeServerBuilder[IO]
       .withHttpWebSocketApp((webSocket: WebSocketBuilder2[IO]) =>
-        CORS(
-          GZip(
-            Router[IO](
-              "/assets" -> StaticResourceRoute("assets"),
-              "/static" -> StaticResourceRoute("static"),
-              "/"       -> WebHtmlRoute("LOCAL"),
-              "/auth"   -> AuthControllerRoutes.routes(InsecureAuthController),
-              "/play"   -> GameControllerRoutes.routes(InsecureAuthController, gameController, webSocket)
-            )
-          ).orNotFound,
-          CORSConfig.default.withAllowCredentials(false)
-        )
+        GZip(
+          Router[IO](
+            "/assets" -> StaticResourceRoute("assets"),
+            "/static" -> StaticResourceRoute("static"),
+            "/"       -> WebHtmlRoute("LOCAL"),
+            "/auth"   -> AuthControllerRoutes.routes(InsecureAuthController),
+            "/play"   -> GameControllerRoutes.routes(InsecureAuthController, gameController, webSocket)
+          )
+        ).orNotFound,
       )
       .bindHttp(9090)
       .resource
@@ -42,4 +37,3 @@ object BackendApp extends IOApp:
 
   override def run(args: List[String]): IO[ExitCode] =
     resource.use(_.compile.drain.as(ExitCode.Success))
-end BackendApp
